45466addd9c5fcba4ef26441c9462021b1f23e0f
Ali Magdy <166527591+alimagdye@users.noreply.github.com>
2025-02-18 04:47:37 +0200

    Update globalMiddleware.js

--- backend/middleware/globalMiddleware.js
+++ backend/middleware/globalMiddleware.js
@@ -3,7 +3,11 @@ import cors from "cors";
 import express from "express";
 
 export const globalMiddleware = [
-  cors(), // Enable CORS (Cross-Origin Resource Sharing) for all origins
+cors({
+    origin: ["https://meow-chat.netlify.app"], // Netlify & local frontend
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow authorization headers & cookies
+  }),
   morgan("dev"), // Log HTTP requests in "dev" format
   express.json(), // Parse incoming JSON requests
   express.urlencoded({ extended: true }),
